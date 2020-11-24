8236e0071f795e7534dcbb053bcdaf524d650f19
npsyco <christian.nymark@gmail.com>
2020-11-24 15:20:51 +0100

    - refactored ColorController.java

--- src/main/java/com/example/stormrider_2020/controller/ColorController.java
+++ src/main/java/com/example/stormrider_2020/controller/ColorController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,42 +21,66 @@ public class ColorController
     @Autowired
     ColorRepository colorRepository;
 
-    @Autowired
-    ColorService colorService;
-
 
     @GetMapping("/color")
-    public ResponseEntity<List<Color>> getAllColor(@RequestParam(required = false) int colorId)
-    {
-        return colorService.getAll(colorId);
+    public ResponseEntity<List<Color>> getAllColor(){
+        try {
+            List<Color> colors = new ArrayList<>();
+            colors = colorRepository.findAll();
+
+            if (colors.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(colors, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @GetMapping("/color{id}")
-    public ResponseEntity<Color> getColorById(@PathVariable("id") long id)
-    {
-        return colorService.getById(id);
+    public ResponseEntity<Color> getColorById(long id){
+        Optional<Color> colorData = colorRepository.findById(id);
+        if (colorData.isPresent())
+            return new ResponseEntity<>(colorData.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
     @PostMapping("/color")
-    public ResponseEntity<Color> createColor(@RequestBody Color color)
-    {
-        return colorService.create(color);
+    public ResponseEntity<Color> createColor(@RequestBody Color color){
+        try {
+            Color color1 = colorRepository.save(color);
+            return new ResponseEntity<>(color1, HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @PutMapping("/color_id{id}")
-    public ResponseEntity<Color> updateColor(@PathVariable("id") long id, @RequestBody Color color)
-    {
-        return colorService.update(id, color);
+    public ResponseEntity<Color> updateColor(@PathVariable("id") long id, @RequestBody Color color){
+        Optional<Color> colorData = colorRepository.findById(id);
+
+        if (colorData.isPresent()){
+            Color color1 = colorData.get();
+            color1.setColorId(color.getColorId());
+            return new ResponseEntity<>(colorRepository.save(color1), HttpStatus.OK);
+        } else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
     @DeleteMapping("/color_id{id}")
-    public ResponseEntity<HttpStatus> deleteColor(@PathVariable("id") long id)
-    {
-        return colorService.delete(id);
+    public ResponseEntity<HttpStatus> deleteColor(@PathVariable("id") long id){
+        try {
+            colorRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
