5c0919cf9467367d22792e8d1c7d683c6713f02d
npsyco <christian.nymark@gmail.com>
2020-11-24 15:41:01 +0100

    - refactored CartController.java

--- src/main/java/com/example/stormrider_2020/controller/CartController.java
+++ src/main/java/com/example/stormrider_2020/controller/CartController.java
@@ -2,7 +2,6 @@ package com.example.stormrider_2020.controller;
 
 import com.example.stormrider_2020.model.Cart;
 import com.example.stormrider_2020.repository.CartRepository;
-import com.example.stormrider_2020.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,52 +19,74 @@ public class CartController
     @Autowired
     CartRepository cartRepository;
 
-    @Autowired
-    CartService cartService;
-
 
     @GetMapping("/cart")
-    public ResponseEntity<List<Cart>> getAllCart(@RequestParam(required = false) int cartId)
-    {
-        public ResponseEntity<List<Cart>> getAllCart(int cartId) {
-        try
-        {
+    public ResponseEntity<List<Cart>> getAllCart(){
+         try{
             List<Cart> carts = new ArrayList<>();
+            carts = cartRepository.findAll();
 
-            if(cartId == 0)
-                carts = cartRepository.findAll();
-            else
-                carts = cartRepository.getCartByCartId(cartId);
-            if (carts.isEmpty())
+            if(carts.isEmpty()){
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+                return new ResponseEntity<>(HttpStatus.OK);
+         } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+         }
+    }
 
-            return new ResponseEntity<>(carts, HttpStatus.OK);
-        }
-        catch (Exception e)
-        {
+
+    @GetMapping("/cart{id}")
+    public ResponseEntity<Cart> getCartById(long id){
+        Optional<Cart> cartData = cartRepository.findById(id);
+        if (cartData.isPresent())
+            return new ResponseEntity<>(cartData.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+    @PostMapping("/cart")
+    public ResponseEntity<Cart> createCart(@RequestBody Cart cart){
+        try {
+            Cart cart1 = cartRepository.save(cart);
+            return new ResponseEntity<>(cart1, HttpStatus.CREATED);
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
 
-    @GetMapping("/cart{id}")
-    public ResponseEntity<Cart> getCartById(@PathVariable("id") long id)
-    {
-        return cartService.getById(id);
-    }
+    @PutMapping("/cart_id{id}")
+    public ResponseEntity<Cart> updateCart(@PathVariable("id") long id, @RequestBody Cart cart){
+        Optional<Cart> cartData = cartRepository.findById(id);
 
-    // see notes in service please
-    @PostMapping("/cart_id{id}")
-    public ResponseEntity<Cart> updateCart(@PathVariable("id") long id, @RequestBody Cart cart)
-    {
-        return cartService.update(id, cart);
+        if (cartData.isPresent()){
+            Cart cart1 = cartData.get();
+            cart1.setStatus(cart.getStatus());
+            cart1.setTotalPrice(cart.getTotalPrice());
+            cart1.setVat(cart.getVat());
+            cart1.setTotalDiscount(cart.getTotalDiscount());
+            cart1.setCreated(cart.getCreated());
+            cart1.setVoucherId(cart.getVoucherId());
+            cart1.setTrackingNumber(cart.getTrackingNumber());
+            cart1.setAddressByBillingAddressId(cart.getAddressByBillingAddressId());
+            cart1.setAddressByShippingAddressId(cart.getAddressByShippingAddressId());
+            cart1.setCustomerByCustomerId(cart.getCustomerByCustomerId());
+            return new ResponseEntity<>(cartRepository.save(cart1), HttpStatus.OK);
+        } else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
     @DeleteMapping("/cart_id{id}")
-    public ResponseEntity<HttpStatus> deleteCart(@PathVariable("id") long id)
-    {
-        return cartService.delete(id);
+    public ResponseEntity<HttpStatus> deleteCart(@PathVariable("id") long id){
+        try {
+            cartRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
