a6de682ebd8150c0a8e25849c18fcc576f6ca18c
npsyco <christian.nymark@gmail.com>
2020-11-24 15:09:31 +0100

    - refactored CustomerController.java

--- src/main/java/com/example/stormrider_2020/controller/CustomerController.java
+++ src/main/java/com/example/stormrider_2020/controller/CustomerController.java
@@ -2,12 +2,13 @@ package com.example.stormrider_2020.controller;
 
 import com.example.stormrider_2020.model.Customer;
 import com.example.stormrider_2020.repository.CustomerRepository;
-import com.example.stormrider_2020.services.CustomerService;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,44 +20,72 @@ public class CustomerController
     @Autowired
     CustomerRepository customerRepository;
 
-    @Autowired
-    CustomerService customerService;
 
-
-    //service not accepting param of customerId ???
     @GetMapping("/customer")
-    public ResponseEntity<List<Customer>> getAllCustomer(@RequestParam(required = false) int customerId)
-    {
-        return customerService.getAll(customerId);
+    public ResponseEntity<List<Customer>> getAllCustomer(){
+        try {
+            List<Customer> customers = new ArrayList<>();
+            customers = customerRepository.findAll();
+
+            if (customers.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(customers, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @GetMapping("/customer{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable("id") long id)
-    {
-        return customerService.getById(id);
+    public ResponseEntity<Customer> getCustomerById(long id){
+        Optional<Customer> customerData = customerRepository.findById(id);
+        if (customerData.isPresent())
+            return new ResponseEntity<>(customerData.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
     @PostMapping("/customer")
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer)
-    {
-        return customerService.create(customer);
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+        try {
+            Customer customer1 = customerRepository.save(customer);
+            return new ResponseEntity<>(customer1, HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
 
     @PutMapping("/customer{id}")
-    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") long id, @RequestBody Customer customer)
-    {
-        return customerService.update(id, customer);
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") long id, @RequestBody Customer customer){
+        Optional<Customer> customerData = customerRepository.findById(id);
+
+        if (customerData.isPresent()){
+            Customer customer1 = customerData.get();
+            customer1.setFirstName(customer.getFirstName());
+            customer1.setLastName(customer.getLastName());
+            customer1.setEmail(customer.getEmail());
+            customer1.setPhone(customer.getPhone());
+            customer1.setCvrNumber(customer.getCvrNumber());
+            customer1.setCompanyName(customer.getCompanyName());
+            return new ResponseEntity<>(customerRepository.save(customer1), HttpStatus.OK);
+        } else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
     @DeleteMapping("/customer{id}")
-    public ResponseEntity<HttpStatus> deleteCustomer(@PathVariable("id") long id)
-    {
-        return customerService.delete(id);
+    public ResponseEntity<HttpStatus> deleteCustomer(@PathVariable("id") long id){
+        try {
+            customerRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
