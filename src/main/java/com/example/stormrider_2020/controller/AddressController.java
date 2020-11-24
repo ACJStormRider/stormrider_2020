b98b58835bc42b987d5de9af35e47475df535d5f
npsyco <christian.nymark@gmail.com>
2020-11-24 15:55:31 +0100

    - refactored AddressController.java

--- src/main/java/com/example/stormrider_2020/controller/AddressController.java
+++ src/main/java/com/example/stormrider_2020/controller/AddressController.java
@@ -3,7 +3,6 @@ package com.example.stormrider_2020.controller;
 
 import com.example.stormrider_2020.model.Address;
 import com.example.stormrider_2020.repository.AddressRepository;
-import com.example.stormrider_2020.services.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,37 +19,70 @@ public class AddressController{
     @Autowired
     AddressRepository addressRepository;
 
-    @Autowired
-    AddressService addressService;
-
 
     @GetMapping("/address")
     public ResponseEntity<List<Address>> getAllAddress(@RequestParam(required = false) int addressId){
-            return addressService.getAll(addressId);
+        try {
+            List<Address> addresses = new ArrayList<>();
+            addresses = addressRepository.findAll();
+
+            if (addresses.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(addresses, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @GetMapping("/address{id}")
-    public ResponseEntity<Address> getAddressById(@PathVariable("id") long id){
-        return addressService.getById(id);
+    public ResponseEntity<Address> getAddressById(long id){
+        Optional<Address> addressData = addressRepository.findById(id);
+        if (addressData.isPresent())
+            return new ResponseEntity<>(addressData.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
     @PostMapping("/address")
     public ResponseEntity<Address> createAddress(@RequestBody Address address){
-        return addressService.create(address);
+        try {
+            Address address1 = addressRepository.save(address);
+            return new ResponseEntity<>(address1, HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @PutMapping("/address_id{id}")
     public ResponseEntity<Address> updateAddress(@PathVariable("id") long id, @RequestBody Address address){
-        return addressService.update(id, address);
+        Optional<Address> addressData = addressRepository.findById(id);
+
+        if (addressData.isPresent()){
+            Address address1 = addressData.get();
+            address1.setAddress(address.getAddress());
+            address1.setCity(address.getCity());
+            address1.setZip(address.getZip());
+            address1.setCounty(address.getCounty());
+            address1.setCountry(address.getCountry());
+            return new ResponseEntity<>(addressRepository.save(address1), HttpStatus.OK);
+        } else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
     @DeleteMapping("/address_id/{id}")
     public ResponseEntity<HttpStatus> deleteAddress(@PathVariable("id") long id){
-        return addressService.delete(id);
+        try {
+            addressRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
