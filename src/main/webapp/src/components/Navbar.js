e3787674661ff6e76888a44c4b7a4546b1dac1dc
Juste <juste.dilyte@gmail.com>
2020-12-04 22:03:51 +0100

    Installed React Bootstrap

--- src/main/webapp/src/components/Navbar.js
+++ src/main/webapp/src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import logo from '../images/logo/dummyLogo.png';
-
+import '../App.css';
 
 export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <img src={logo} alt="Logo" />;
+                <div>
+                    <img src={logo} alt="Logo" className="logo" />
+                </div>
+                <div className="navbar">
+                    <span>Riding Gear</span>
+                    <span>Other Stuff</span>
+                    <span>About Us</span>
+                </div>
             </div>
         )
     }
