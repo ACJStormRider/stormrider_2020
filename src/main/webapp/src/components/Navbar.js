44ea0ae11ded0969bc685e2854fca2a7aeecde48
Juste <juste.dilyte@gmail.com>
2020-12-07 17:11:11 +0100

    Installed React Bootstrap

--- src/main/webapp/src/components/Navbar.js
+++ src/main/webapp/src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
 import logo from '../images/logo/dummyLogo.png';
 import '../App.css';
+import axios from 'axios';
 
 export default class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories : [] // constructs a list for categories
+        }
+    }
+
+    componentDidMount() {
+        axios.get("") // link to the localhost url from controller
+            .then((data) => {
+                this.setState({categories: data});
+            });
+    }
+
+
     render() {
         return (
             <div>
